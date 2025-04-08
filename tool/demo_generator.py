bd3050d324705e6d30e6be35788927075c86b65c
Qirun Wang <92351559+QirunWang@users.noreply.github.com>
2025-04-08 16:16:57 +0800

    Add files via upload

--- tool/demo_generator.py
+++ tool/demo_generator.py
@@ -22,15 +22,27 @@ class demo_input:
                  aPos_mask_frac = 0,  # mask fraction of aPos
                  aExpr_mask_frac = 0.15,  #mask fraction of aExpr
 
+                 addPadding=True,  # whether to add padding if the non-zero lenght < max length
                  max_gene_num=1000, # gene ID range
                  max_expr=10,       # RNA and ATAC log1p expr range
-                 max_pos=1e6):      # ATAC postion range
+                 max_pos=1e6,       # ATAC postion range
+
+                 addStat = True,    # whether to add statistic infomation
+                 stat_dim = 7,      # statdim, [btz, seq_len, stat_dim]
+                 max_stat = 10,     # max stat
+
+                 device=None):
         """
         Initializes the generator with:
         :param n_batch: number of elements in the minibatch
         :param gene_len: length for gene-related lists (gID, gExpr, gPos)
         :param atac_len: length for atac-related lists (aPos, aExpr)
         """
+        if device is None:
+            self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        else:
+            self.device = device
+
         self.n_batch = n_batch
         self.gene_len = gene_len
         self.atac_len = atac_len
@@ -41,9 +53,14 @@ class demo_input:
         self.aPos_mask_frac = aPos_mask_frac
         self.aExpr_mask_frac = aExpr_mask_frac
 
+        self.addPadding = addPadding
         self.max_gene_num = max_gene_num
         self.max_expr = max_expr
         self.max_pos = int(max_pos)
+
+        self.addStat = addStat
+        self.stat_dim = stat_dim
+        self.max_stat = max_stat
 
     def _pad_or_crop(self, tensor, total_len):
         """Pad with zeros if tensor length is less than total_len, or crop if longer."""
@@ -54,6 +71,32 @@ class demo_input:
         else:
             tensor = tensor[:total_len]
         return tensor
+
+    def _crop(self, tensor, total_len):
+        """crop if the tensor length is longer than total_len."""
+        current_len = tensor.size(0)
+        if current_len > total_len:
+            tensor = tensor[:total_len]
+        return tensor
+
+    def add_stat(self):
+        batch_gStat = []
+        for _ in range(self.n_batch):
+            # gStat: random floats between 0 and max_stat
+            temp2 = []
+            for i in range(self.stat_dim):
+                temp1 = torch.rand(self.gene_len) * self.max_stat
+                if self.addPadding:
+                    temp1 = self._pad_or_crop(temp1, self.gene_total_len)
+                else:
+                    temp1 = self._crop(temp1, self.gene_total_len)
+                temp2.append(temp1)
+            gStat = torch.stack(temp2, dim=1)
+            batch_gStat.append(gStat)
+
+        # Stack the tensors to form the final batch with shapes: [btz, seq_len, stat_dim]
+        batch_gStat = torch.stack(batch_gStat)
+        return batch_gStat
 
     def generate_data(self):
         batch_gID = []
@@ -72,40 +115,64 @@ class demo_input:
             if self.gene_len > self.max_gene_num:
                 raise ValueError("gene_len cannot be greater than max_gene_num for unique values.")
             gID = torch.randperm(self.max_gene_num)[:self.gene_len]
-            gID = self._pad_or_crop(gID, self.gene_total_len)
+            if self.addPadding:
+                gID = self._pad_or_crop(gID, self.gene_total_len)
+            else:
+                gID = self._crop(gID, self.gene_total_len)
 
             # gPos: unique random integers between 1 and max_pos (inclusive)
             if self.gene_len > self.max_pos:
                 raise ValueError("gene_len cannot be greater than max_pos for unique gene positions.")
             gPos = torch.randperm(self.max_pos)[:self.gene_len] + 1
-            gPos = self._pad_or_crop(gPos, self.gene_total_len)
+            if self.addPadding:
+                gPos = self._pad_or_crop(gPos, self.gene_total_len)
+            else:
+                gPos = self._crop(gPos, self.gene_total_len)
 
             # gExpr: random floats between 0 and max_expr
             gExpr = torch.rand(self.gene_len) * self.max_expr
-            gExpr = self._pad_or_crop(gExpr, self.gene_total_len)
+            if self.addPadding:
+                gExpr = self._pad_or_crop(gExpr, self.gene_total_len)
+            else:
+                gExpr = self._crop(gExpr, self.gene_total_len)
 
             # gExpr_mask: binary mask with probability gExpr_mask_frac for 1
             gExpr_mask = (torch.rand(self.gene_len) < self.gExpr_mask_frac).to(torch.int64)
-            gExpr_mask = self._pad_or_crop(gExpr_mask, self.gene_total_len)
+            if self.addPadding:
+                gExpr_mask = self._pad_or_crop(gExpr_mask, self.gene_total_len)
+            else:
+                gExpr_mask = self._crop(gExpr_mask, self.gene_total_len)
 
             # ATAC-related tensors
             # aPos: unique random integers between 1 and max_pos (inclusive)
             if self.atac_len > self.max_pos:
                 raise ValueError("atac_len cannot be greater than max_pos for unique atac positions.")
             aPos = torch.randperm(self.max_pos)[:self.atac_len] + 1
-            aPos = self._pad_or_crop(aPos, self.atac_total_len)
+            if self.addPadding:
+                aPos = self._pad_or_crop(aPos, self.atac_total_len)
+            else:
+                aPos = self._crop(aPos, self.atac_total_len)
 
             # aPos_mask: binary mask with probability aPos_mask_frac for 1
             aPos_mask = (torch.rand(self.atac_len) < self.aPos_mask_frac).to(torch.int64)
-            aPos_mask = self._pad_or_crop(aPos_mask, self.atac_total_len)
+            if self.addPadding:
+                aPos_mask = self._pad_or_crop(aPos_mask, self.atac_total_len)
+            else:
+                aPos_mask = self._crop(aPos_mask, self.atac_total_len)
 
             # aExpr: random floats between 0 and max_expr
             aExpr = torch.rand(self.atac_len) * self.max_expr
-            aExpr = self._pad_or_crop(aExpr, self.atac_total_len)
+            if self.addPadding:
+                aExpr = self._pad_or_crop(aExpr, self.atac_total_len)
+            else:
+                aExpr = self._crop(aExpr, self.atac_total_len)
 
             # aExpr_mask: binary mask with probability aExpr_mask_frac for 1
             aExpr_mask = (torch.rand(self.atac_len) < self.aExpr_mask_frac).to(torch.int64)
-            aExpr_mask = self._pad_or_crop(aExpr_mask, self.atac_total_len)
+            if self.addPadding:
+                aExpr_mask = self._pad_or_crop(aExpr_mask, self.atac_total_len)
+            else:
+                aExpr_mask = self._crop(aExpr_mask, self.atac_total_len)
 
             batch_gID.append(gID)
             batch_gPos.append(gPos)
@@ -130,18 +197,35 @@ class demo_input:
         batch_aExpr = torch.stack(batch_aExpr)
         batch_aExpr_mask = torch.stack(batch_aExpr_mask)
 
-        RNA_module = {
-            "gID": batch_gID,
-            "gPos": batch_gPos,
-            "gExpr": batch_gExpr,
-            "gExpr_mask": batch_gExpr_mask,}
+        if self.addStat:
+            batch_gStat = self.add_stat()
+            RNA_module = {
+                "gID": batch_gID.to(self.device),
+                "gPos": batch_gPos.to(self.device),
+                "gExpr": batch_gExpr.to(self.device),
+                "gExpr_mask": batch_gExpr_mask.to(self.device),
+                "gStat": batch_gStat.to(self.device)
+            }
+            ATAC_module = {
+                "aPos": batch_aPos.to(self.device),
+                "aPos_mask": batch_aPos_mask.to(self.device),
+                "aExpr": batch_aExpr.to(self.device),
+                "aExpr_mask": batch_aExpr_mask.to(self.device)
+            }
+        else:
+            RNA_module = {
+                "gID": batch_gID.to(self.device),
+                "gPos": batch_gPos.to(self.device),
+                "gExpr": batch_gExpr.to(self.device),
+                "gExpr_mask": batch_gExpr_mask.to(self.device)
+            }
+            ATAC_module = {
+                "aPos": batch_aPos.to(self.device),
+                "aPos_mask": batch_aPos_mask.to(self.device),
+                "aExpr": batch_aExpr.to(self.device),
+                "aExpr_mask": batch_aExpr_mask.to(self.device)
+            }
 
-        ATAC_module = {
-            "aPos": batch_aPos,
-            "aPos_mask": batch_aPos_mask,
-            "aExpr": batch_aExpr,
-            "aExpr_mask": batch_aExpr_mask
-        }
         return RNA_module, ATAC_module
 
 # Example usage:
@@ -155,9 +239,11 @@ if __name__ == "__main__":
         gExpr_mask_frac=0.15,
         aPos_mask_frac=0,
         aExpr_mask_frac=0.15,
+        addPadding=False,
         max_gene_num=1000,
         max_expr=10,
-        max_pos=1000000  # Using an integer value
+        max_pos=1000000,  # Using an integer value
+        device = None
     )
     RNA_module, ATAC_module = generator.generate_data()
 
